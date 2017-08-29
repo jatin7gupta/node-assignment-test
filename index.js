327defb6beae50bf8f571ccac7aa20b6870fdb63
jatin7gupta <jatin13csu055@ncuindia.edu>
2017-08-29 14:31:35 +0530

    more get methods

--- index.js
+++ index.js
@@ -12,6 +12,40 @@ app.use((req, res, next) => {
     next();
 });
 
+app.get('/api/todos/:status', (req, res) => {
+    'use strict';
+    const status = req.params.status;
+    if (status === 'Active') {
+        const activeTodos = {};
+        for (const key of Object.keys(db.DB)) {
+            if (db.DB[key].status === 'Active') {
+                activeTodos[key] = db.DB[key];
+            }
+        }
+        res.json(activeTodos);
+    }
+    if (status === 'Complete') {
+        const completeTodos = {};
+        for (const key of Object.keys(db.DB)) {
+            if (db.DB[key].status === 'Complete') {
+                completeTodos[key] = db.DB[key];
+            }
+        }
+        res.json(completeTodos);
+    }
+    if (status === 'Deleted') {
+        const deletedTodos = {};
+        for (const key of Object.keys(db.DB)) {
+            if (db.DB[key].status === 'Deleted') {
+                deletedTodos[key] = db.DB[key];
+            }
+        }
+        res.json(deletedTodos);
+    } else {
+        res.json('Invalid Request', 400);
+    }
+});
+
 app.get('/api/todos', (req, res) => {
     'use strict';
     res.json(db.DB);
@@ -50,6 +84,25 @@ app.post('/api/todos', (req, res) => {
         db.DB[db.nextTodoID] = newTodoObject;
         db.nextTodoID += 1;
         res.send('Successfully added');
+    }
+});
+
+app.put('/api/todos/:id', (req, res) => {
+    'use strict';
+    const modId = req.params.id;
+    const todo = db.DB[modId];
+    if (todo === undefined) {
+        res.json('Invalid Todo value', 400);
+    } else {
+        const title = req.body.title;
+        if (title && todo !== '' && title.trim() !== '') {
+            todo.title = title;
+        }
+        const todoStatus = req.body.status;
+        if (todoStatus && (todoStatus === db.status.Active || todoStatus === db.status.Complete)) {
+            todo.status = todoStatus;
+        }
+        res.json(todo);
     }
 });
 
